ef530fce38a754d25d62d77bb9969227004c1313
Kaplaugher <kaplaugher94@gmail.com>
2017-08-10 11:38:16 -0400

    fetched memes and basic css

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,23 @@
-export const RECIEVE_MEMES = 'RECIEVE_MEMES';
 
-function recieveMemes(json) {
+export const RECEIVE_MEMES = 'RECEIVE_MEMES';
+
+function receiveMemes(json) {
   const { memes } = json.data;
 
   return {
-    type: RECIEVE_MEMES,
+    type: RECEIVE_MEMES,
     memes
+  }
+}
+
+function fetchMemesJson() {
+  return fetch('https://api.imgflip.com/get_memes')
+  .then(response => response.json())
+}
+
+export function fetchMemes() {
+  return function(dispatch) {
+    return fetchMemesJson()
+    .then(json => dispatch(receiveMemes(json)))
   }
 }
